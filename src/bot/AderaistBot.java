1526eac17674f713cb6ac478a4b2d538daaae613
Jaernbrand <henrikjarnbrand@gmail.com>
2016-06-01 14:34:06 +0200

    Increase code readability

--- src/bot/AderaistBot.java
+++ src/bot/AderaistBot.java
@@ -1,5 +1,6 @@
 package bot;
 
+import java.util.Collection;
 import java.util.EnumMap;
 import java.util.HashSet;
 import java.util.List;
@@ -12,11 +13,12 @@ import gameLogic.Direction;
 import gameLogic.GameState;
 import gameLogic.Position;
 import gameLogic.Snake;
-import javafx.geometry.Pos;
 
 public class AderaistBot implements Brain
 {
 
+	private int STD_MAX_DEPTH = 16;
+			
 	private GameState gamestate;
 	private Snake self;
 	private Map<Direction, Integer> safestDirection;
@@ -65,7 +67,9 @@ public class AderaistBot implements Brain
 	private Direction getDirectionToClosesFruit(List<Position> fruits)
 	{
 		Position closestFruitPosition = getClosestFruit(fruits);
-		List<Direction> wayToFruit = GameState.getRelativeDirections(self.getHeadPosition(), closestFruitPosition);
+		List<Direction> wayToFruit = GameState.getRelativeDirections(self.getHeadPosition(), 
+				                                                     closestFruitPosition);
+		
 		Direction directionToFruit = wayToFruit.get(0);
 		if(directionIsOppositeToCurrentDirection(directionToFruit) && wayToFruit.size() == 2)
 			directionToFruit = wayToFruit.get(1);
@@ -85,12 +89,12 @@ public class AderaistBot implements Brain
 		return false;
 	}
 
-	private int turnsUntilCollision(Direction direction, int turns, Set<Position> lethalObstacles)
+	private int turnsUntilCollision(Direction direction, int turns, Set<Position> lethalObstacles, int max_depth)
 	{
 		if(turns < 1)
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("No going back in time!");
 		
-		if(turns == 16)
+		if(turns == max_depth)
 			return turns;
 		
 		if(directionIsOppositeToCurrentDirection(direction))
@@ -104,13 +108,7 @@ public class AderaistBot implements Brain
 				lethalObstacles.addAll(snake.getSegments());
 		}
 		
-		for(Snake snake : snakes)
-		{
-			if(snake.equals(self) || snake.isDead())
-				continue;
-			Position futurePosition = getFuturePosition(snake, turns);
-			lethalObstacles.add(futurePosition);
-		}
+		fillWithFuturePositions(snakes, lethalObstacles, turns);
 		
 		Position myFuturePosition =	getFuturePosition(direction, self, turns);
 		
@@ -120,7 +118,20 @@ public class AderaistBot implements Brain
 		if(outOfBounds(myFuturePosition))
 			return turns;
 		
-		return turnsUntilCollision(direction, ++turns, lethalObstacles);
+		return turnsUntilCollision(direction, ++turns, lethalObstacles, max_depth);
+	}
+	
+	private void fillWithFuturePositions(Collection<Snake> snakes, 
+			Collection<Position> lethalObstacles,
+			int turns)
+	{
+		for(Snake snake : snakes)
+		{
+			if(snake.equals(self) || snake.isDead())
+				continue;
+			Position futurePosition = getFuturePosition(snake, turns);
+			lethalObstacles.add(futurePosition);
+		}
 	}
 
 	private Position getFuturePosition(Snake snake, int turns)
@@ -140,6 +151,9 @@ public class AderaistBot implements Brain
 	private boolean outOfBounds(Position position)
 	{
 		Board board = gamestate.getBoard();
+		// A lethal wall covers the edges of the board. The wall is one square 
+		// wide. A marign of one square is therefore needed to not collide with 
+		// the wall.
 		if(position.getX() < 1 || position.getY() < 1)
 			return true;
 		if(position.getX() >= board.getWidth() - 1 || position.getY() >= board.getHeight() -1)
@@ -149,12 +163,21 @@ public class AderaistBot implements Brain
 
 	private Direction nextTurn()
 	{
+		gatherTurnsUntilCollisionForDirections();
+		return safestDirection();
+	}
+	
+	private void gatherTurnsUntilCollisionForDirections()
+	{
 		for(Direction dir : Direction.values())
 		{
-			final int turns = turnsUntilCollision(dir, 1, new HashSet<Position>(128));
+			final int turns = turnsUntilCollision(dir, 1, new HashSet<Position>(128), STD_MAX_DEPTH);
 			safestDirection.put(dir, turns);
 		}
-		
+	}
+	
+	private Direction safestDirection()
+	{
 		int longest = -1;
 		Direction safest = Direction.SOUTH;
 		
@@ -167,12 +190,11 @@ public class AderaistBot implements Brain
 				safest = dir;
 			}
 		}
-		
 		return safest;
 	}
 	
 	private boolean willCollide(Direction direction)
 	{
-		return turnsUntilCollision(direction, 1, new HashSet<Position>(128)) < 3;
+		return turnsUntilCollision(direction, 1, new HashSet<Position>(128), STD_MAX_DEPTH) < 3;
 	}
 }
